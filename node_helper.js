c5b0c3679446d3a5a39babba3046e617e10893fe
theTapp-astro <lisa.a.vicars@gmail.com>
2026-08-12 08:27:13 -0400

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -269,7 +269,14 @@ module.exports = NodeHelper.create({
 				const projectName =
 					projectMap.get(String(task.project_id)) ||
 					"";
-
+			
+				Log.info(
+					"[MMM-TodoistInteractive] Task: " +
+					task.content +
+					" | assignee_id: " +
+					task.assignee_id
+				);
+			
 				return {
 					...task,
 					projectName
